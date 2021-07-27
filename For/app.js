8591ff4d4518f5e10f7e1c0b2e0ded092684761d
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2021-07-27 12:35:49 +0600

    Add files via upload

---
+++ For/app.js
@@ -0,0 +1,31 @@
+function even(num1,num2) {
+    document.getElementById("ev_h3").style.display = "block";
+    for (;num1<num2; num1++) {
+
+        if(num1%2==0){
+            var node = document.createElement("LI");                 // Create a <li> node
+            var textnode = document.createTextNode(num1);         // Create a text node
+            node.appendChild(textnode);                              // Append the text to <li>
+            document.getElementById("even").appendChild(node);
+        }
+
+    
+    }
+
+}
+
+
+
+function odd(num1,num2) {
+    document.getElementById("odd_h3").style.display = "block";
+    for (;num1<num2; num1++) {
+        if (num1%2!=0) {
+            var node = document.createElement("LI");                 // Create a <li> node
+            var textnode = document.createTextNode(num1);         // Create a text node
+            node.appendChild(textnode);                              // Append the text to <li>
+            document.getElementById("odd").appendChild(node);
+        }
+    
+    }
+
+}
